09c505d66a4d6f2ba84c3aad22d4b4fb61a457f3
minsi yang <yangminsi@gmail.com>
2020-07-20 13:32:29 +0100

    second test added for string with two elements, passing

--- lib/summary.rb
+++ lib/summary.rb
@@ -1,3 +1,5 @@
 def summary(grade)
-  "Green: 1"
+  w = grade.split(",")
+  s = grade.split(",").count
+  return "#{w[0]}: #{s}"
 end
--- spec/summary_spec.rb
+++ spec/summary_spec.rb
@@ -4,4 +4,8 @@ describe "#summary" do
   it "return 'Green: 1' when passed 'Green' " do
     expect(summary('Green')).to eq('Green: 1')
   end
+
+  it "return 'Green: 2' when passed 'Green, Green' " do
+    expect(summary('Green, Green')).to eq('Green: 2')
+  end
 end
