50cc31cf9e6107d40cc53171f3f8f04e89ccc239
minsi yang <yangminsi@gmail.com>
2020-08-14 15:10:55 +0100

    Added test for argument 'Red', passed

--- lib/grade.rb
+++ lib/grade.rb
@@ -3,6 +3,8 @@ class Grade
     arr = string.split(", ")
     if arr.include?('Amber')
       "Amber: #{arr.count('Amber')}"
+    elsif arr.include?('Red')
+      "Red: #{arr.count('Red')}"
     else
       "Green: #{arr.count('Green')}"
     end
--- spec/grade_spec.rb
+++ spec/grade_spec.rb
@@ -2,16 +2,20 @@ require 'grade'
 
 describe Grade do
   describe "#generate_summary" do
-    it "return 'Geen: 1' when pass 'Green' " do
+    it "return 'Geen: 1' when passed 'Green' " do
       expect(subject.generate_summary('Green')).to eq 'Green: 1'
     end
 
-    it "return 'Green: 2'  when pass 'Green, Green' " do
+    it "return 'Green: 2'  when passed 'Green, Green' " do
       expect(subject.generate_summary('Green, Green')).to eq 'Green: 2'
     end
 
-    it "return 'Amber: 1' when pass 'Amber' " do
+    it "return 'Amber: 1' when passed 'Amber' " do
       expect(subject.generate_summary('Amber')).to eq 'Amber: 1'
+    end
+
+    it "return 'Red: 1' when passed 'Red' " do
+      expect(subject.generate_summary('Red')).to eq 'Red: 1'
     end
 
   end
