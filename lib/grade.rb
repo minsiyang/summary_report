a78f8410d09d94631d8a2e32784584f7a69f6f6c
minsi yang <yangminsi@gmail.com>
2020-08-14 20:50:33 +0100

    Trying to refactored first four tests, fifth one still fail

--- lib/grade.rb
+++ lib/grade.rb
@@ -1,14 +1,21 @@
 class Grade
   def generate_summary(string)
-
+    h = Hash.new(0)
     arr = string.split(", ")
-    
-    if arr.include?('Amber')
-      "Amber: #{arr.count('Amber')}"
-    elsif arr.include?('Red')
-      "Red: #{arr.count('Red')}"
-    else
-      "Green: #{arr.count('Green')}"
+    arr.each do |w|
+      h[w] += 1
     end
+   
+    h.each do |k, v|
+     return "#{k}: #{v}"
+    end
+  
+    # if arr.include?('Amber')
+    #   "Amber: #{arr.count('Amber')}"
+    # elsif arr.include?('Red')
+    #   "Red: #{arr.count('Red')}"
+    # else
+    #   "Green: #{arr.count('Green')}"
+    # end
   end
 end
