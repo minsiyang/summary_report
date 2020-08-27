d6ceee425d5777b0be08b1a07b356cff485671fc
Minsi Yang <yangminsi@gmail.com>
2020-08-27 13:28:53 +0100

    Final refactor of #generate_summary

--- lib/grade.rb
+++ lib/grade.rb
@@ -1,11 +1,6 @@
 class Grade
   def generate_summary(string)
     arr = string.split(", ")
-    if arr.uniq.size == 1
-      s = arr[0]
-      "#{s}: #{arr.count(s)}"
-    else 
-      arr.uniq.map { |x| "#{x}: #{arr.count(x)}"}.join("\n")
-    end
+    arr.uniq.map { |x| "#{x}: #{arr.count(x)}"}.join("\n")
   end
 end
